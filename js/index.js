8e9485e4664287a688374f9ded24d0ba2df39170
Monaco Work <lucas.monaco.maquina@gmail.com>
2020-12-04 16:57:53 -0300

    buffer audio

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 var container = document.querySelector(".container")
 var flockAudio = document.querySelector("#flock-audio");
-var seaAudio = document.querySelector("#sea-audio");
 var wavesAudio = document.querySelector("#waves-audio");
 /* construct manually */
 var bar = new ldBar(".ldBar");
@@ -9,12 +8,21 @@ var bar = new ldBar(".ldBar");
 //     bar.set(50);
 // })
 
+var seaAudio = document.querySelector("#sea-audio");
+seaAudio.addEventListener('timeupdate', function () {
+    var buffer = .44
+    if (this.currentTime > this.duration - buffer) {
+        this.currentTime = 0
+        this.play()
+    }
+});
+
 window.addEventListener("load", function () {
     bar.set(100);
-    gsap.to(".load-txt-1", .6, { opacity: 0, ease: "power2.out", delay: .7});
-    gsap.to(".load-txt-2", .6, { opacity: 1, ease: "power2.out", delay: .8});
+    gsap.to(".load-txt-1", .6, { opacity: 0, ease: "power2.out", delay: .7 });
+    gsap.to(".load-txt-2", .6, { opacity: 1, ease: "power2.out", delay: .8 });
     document.body.addEventListener("click", function () {
-        gsap.to(container, 1, { opacity: 1, ease: "power2.out"});
+        gsap.to(container, 1, { opacity: 1, ease: "power2.out" });
         seaAudio.play();
         seaAudio.volume = 0.2;
         // flockAudio.play();
