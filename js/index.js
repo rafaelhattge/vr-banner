4e80d6c055bda429fd26dcf4d97ee3da8af44ad1
Rafael Ivan Hattge <rafaelhattge@gmail.com>
2020-12-04 03:05:56 -0300

    Optimized textures

--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@ var container = document.querySelector(".container")
 var flockAudio = document.querySelector("#flock-audio");
 var seaAudio = document.querySelector("#sea-audio");
 var wavesAudio = document.querySelector("#waves-audio");
+/* construct manually */
+var bar = new ldBar(".ldBar");
+/* ldBar stored in the element */
+flockAudio.addEventListener("load", function () {
+    bar.set(50);
+})
 window.addEventListener("load", function () {
+    bar.set(100);
     gsap.to(".ready-text", 1, { opacity: 1, ease: "power2.out", delay: .3 });
     document.body.addEventListener("click", function () {
         gsap.to(container, 1, { opacity: 1, ease: "power2.out", delay: .5 });
