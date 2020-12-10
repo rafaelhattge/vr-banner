0a0cafbd896be81d69149ce8d24cadbadb2af762
Rafael Ivan Hattge <rafaelhattge@gmail.com>
2020-12-10 06:51:26 -0300

    Added click animation to cta

--- js/index.js
+++ js/index.js
@@ -7,22 +7,37 @@ window.addEventListener("load", function () {
     bar.set(100);
     gsap.to(".load-txt-1", .6, { opacity: 0, ease: "power2.out", delay: .7 });
     gsap.to(".load-txt-2", .6, { opacity: 1, ease: "power2.out", delay: .8 });
+
     document.body.addEventListener("click", function () {
         if (firstClick) {
             gsap.to(container, 1, { opacity: 1, ease: "power2.out" });
             gsap.to(".loading-screen", .3, { opacity: 0, display: 'none', ease: "power2.out" });
 
-
-            var audio_file = new Audio("../assets/WavesLoops_Agitado_v3_1.mp3");
-            audio_file.play();
-            audio_file.volume = .2;
-            audio_file.addEventListener('timeupdate', function () {
-                var buffer = .44;
-                if (this.currentTime > this.duration - buffer) {
-                    this.currentTime = 0
-                    this.play()
-                }
-            });
+            var audio_file = new Audio("../assets/waves.mp3");
+            // audio_file.play();
+            var playPromise = audio_file.play();
+            if (playPromise !== undefined) {
+                playPromise.then(function () {
+                    audio_file.addEventListener('timeupdate', function () {
+                        // console.log(audio_file.currentTime, audio_file.duration);
+                        var buffer = .44;
+                        if (this.currentTime > this.duration - buffer) {
+                            this.currentTime = 0
+                            this.play()
+                        }
+                    }, true);
+                }).catch(function (error) {
+                    // console.error('Failed to start your sound, retrying.');
+                });
+            }
+            // audio_file.volume = .4;
+            // audio_file.addEventListener('timeupdate', function () {
+            //     var buffer = .44;
+            //     if (this.currentTime > this.duration - buffer) {
+            //         this.currentTime = 0
+            //         this.play()
+            //     }
+            // });
             firstClick = false;
         }
 
